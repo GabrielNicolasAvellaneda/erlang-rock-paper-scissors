f3ce1dbfd52efd5fcb3bbc22be088329df3ee2e9
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-11-21 14:32:28 -0300

    Added other cases for get_best_play
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- rockpaperscissors.erl
+++ rockpaperscissors.erl
@@ -8,13 +8,26 @@ create_play(Player, Played) -> {Player, Played}.
 %get_play(Play) -> element(2, Play). 
 
 get_best_play(Play1 = {_, paper}, _Play2 = {_, rock}) -> Play1;
-get_best_play(_Play1 = {_, rock}, Play2 = {_, paper}) -> Play2.
+get_best_play(_Play1 = {_, rock}, Play2 = {_, paper}) -> Play2;
+get_best_play(_Play1 = {_, paper}, Play2 = {_, scissors}) -> Play2;
+get_best_play(Play1 = {_, scissors}, _Play2 = {_, paper}) -> Play1; 
+get_best_play(Play1 = {_, rock}, _Play2 = {_, scissors}) -> Play1;
+get_best_play(_Play1 = {_, scissors}, Play2 = {_, rock}) -> Play2;
+get_best_play(_, _) -> undefined.
 
 check_result(Play1, Play2) ->
 	{BestPlayPlayer, BestPlayValue} = get_best_play(Play1, Play2),
 	{won, BestPlayPlayer, BestPlayValue}.		
 
 %% Unit tests
+get_best_play_test() ->
+	?assertEqual(get_best_play({player1, rock}, {player2, paper}), {player2, paper}),
+	?assertEqual(get_best_play({player1, scissors}, {player2, paper}), {player1, scissors}),
+	?assertEqual(get_best_play({player1, rock}, {player2, scissors}), {player1, rock}),
+       	?assertEqual(get_best_play({player1, rock}, {player2, rock}), undefined),
+	?assertEqual(get_best_play({player1, paper}, {player2, paper}), undefined),
+	?assertEqual(get_best_play({player1, scissors}, {player2, scissors}), undefined).	
+
 check_result_between_plays_test() ->
 	Play1 = create_play(player1, paper),
 	Play2 = create_play(player2, rock),
